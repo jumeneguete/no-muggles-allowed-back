7cda145c912738a112c462cee6a4d333c861a8cf
Luanna Aquino <luannaluleticia@gmail.com>
2021-06-30 18:53:54 -0300

    database config

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import app from "./app.js";
+const port = process.env.PORT||4000
 
-app.listen(4000, () => {
-    console.log('Server is listening on port 4000.')
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}.`)
 })
